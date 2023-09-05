51a5826b7b0a2430d54d0a8806bbe298138801b2
GuilhermEmanuel2414 <guiemanuel284131@gmail.com>
2023-09-04 21:56:32 -0300

    commit 01 models

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <>
+                        
+            <button> Carros Velhos </button>
+            <button> Carros Esportivos </button>
+            <button> Carros Comuns </button>
+    </>
+  );
+}
+
+export default Navigation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import CarrosVelhos from '../pages/Carros_velhos'
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from '../components/Navigation';
+import CarrosVelhos from './Carros_velhos'
+import CarrosEsportivos from './Carros_esportivos'
+import CarrosComuns from './Carros_comuns'
 
 function Home() {
     return (
         <>
             <label> Este é o nosso projeto! Selecione um tipo de carro abaixo: </label> <p> </p>
+
             <div> 
+                <Navigation />
+                <Route path="/screen1" exact component={CarrosComuns} />
                 <button> Carros Velhos </button>
                 <button> Carros Esportivos </button>
                 <button> Carros Comuns </button>
-
             </div>
         </>
     )
